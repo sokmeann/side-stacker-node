ce782f984aa7848af57d7f0a96f848be59c0bb40
sokmeann <sokmean.nou@gmail.com>
2022-03-29 08:23:36 -0400

    add cors origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://side-stacker-react.herokuapp.com",
     methods: ["GET", "POST"]
   }
 });
